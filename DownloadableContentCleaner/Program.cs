bded925bcde34464eac144ffcc25e7827ed34daa
uros <uros@brainjocks.com>
2018-06-07 14:10:12 +0200

    Initial

---
+++ DownloadableContentCleaner/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadableContentCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!TaskScheduler.TaskExists(@"Downloadable Content Cleaner"))
+            {
+                TaskScheduler.CreateTask();
+            }
+
+            // Upload folder location
+            var uploadFolderPath = @"E:\CustomerOnline\upload"; 
+            var uploadFolder = new DirectoryInfo(uploadFolderPath);
+
+            if (uploadFolder.Exists)
+            {
+                // Get files older then 30 days
+                var dateFilter = DateTime.Now.Date.AddDays(-30);
+                var files =
+                    uploadFolder.GetFiles()
+                        .Where(f => f.CreationTime < dateFilter)
+                        .OrderBy(p => p.CreationTime)
+                        .ToArray();
+                        
+                foreach (var file in files)
+                {
+                    var message = "Deleted " + file.Name + " Created on : " + file.CreationTime;
+                    file.Delete();
+                    LogActivity(message);
+                }
+            }
+        }
+
+        private static void LogActivity(string message)
+        {
+            var appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var fileLocation = appDirectory + "DownloadableContentCleanerLog.txt";
+
+            var log = !File.Exists(fileLocation) ? new StreamWriter(fileLocation) : File.AppendText(fileLocation);
+
+            // Write to the file
+            log.WriteLine(DateTime.Now);
+            log.WriteLine(message);
+            log.WriteLine();
+
+            // Close the stream
+            log.Close();
+        }
+    }
+}
---
+++ DownloadableContentCleaner/TaskScheduler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Win32.TaskScheduler;
+
+namespace DownloadableContentCleaner
+{
+   public class TaskScheduler
+    {
+        public static bool TaskExists(string taskName)
+        {
+            // Get the service on the local machine
+            using (var ts = new TaskService())
+            {
+                return ts.GetTask(taskName) != null;
+            }
+        }
+
+        public static void CreateTask()
+        {
+            const string name = @"Downloadable Content Cleaner";
+            const string description = @"Schedule a Downloadable Content Cleaning task";
+
+            using (var ts = new TaskService())
+            {
+                var taskDefinition = ts.NewTask();
+                taskDefinition.RegistrationInfo.Description = description;
+
+                // Create daily trigger
+                var daily = new DailyTrigger
+                {
+                    StartBoundary = Convert.ToDateTime(DateTime.Today.ToShortDateString() + " 17:30:00"),
+                    DaysInterval = 1
+                };
+
+                taskDefinition.Triggers.Add(daily);
+
+                // App location
+                var appLocation = AppDomain.CurrentDomain.BaseDirectory + "DownloadableContentCleaner.exe";
+                taskDefinition.Actions.Add(new ExecAction(appLocation));
+
+                ts.RootFolder.RegisterTaskDefinition(name, taskDefinition,
+                    TaskCreation.CreateOrUpdate, "SYSTEM", null,
+                    TaskLogonType.ServiceAccount);
+            }
+        }
+    }
+}
